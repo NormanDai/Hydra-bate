7d43abdaffea42197c2786fa5fc877205e8fb108
Will <yao_jin90@163.com>
2017-08-24 15:03:49 +0800

    exp

---
+++ expression/src/com/heshuai/CronExpressionEx.java
@@ -0,0 +1,143 @@
+package com.heshuai;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Set;
+import java.util.StringTokenizer;
+import org.quartz.*;
+
+
+
+public class CronExpressionEx extends CronExpression
+{
+	public static final Integer ALL_SPEC = new Integer(ALL_SPEC_INT);
+	public static final int NO_SPEC_INT = 98; // '?'
+
+	private String secondsExp;
+	private String minutesExp;
+	private String hoursExp;
+	private String daysOfMonthExp;
+	private String monthsExp;
+	private String daysOfWeekExp;
+
+	public static void main(String[] args) {
+
+		try {
+			CronExpressionEx exp = new CronExpressionEx("0 0 10,14,16 * * ? ");
+
+			Date dd = new Date();
+			for (int i = 1; i <= 8; i++)
+			{
+				dd = exp.getNextValidTimeAfter(dd);
+				System.out.println(dd);
+				dd = new java.util.Date(dd.getTime() + 1000);
+
+			}
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public CronExpressionEx(String cronExpression) throws ParseException
+	{
+		super(cronExpression);
+
+		StringTokenizer exprsTok = new StringTokenizer(cronExpression, " \t", false);
+		secondsExp = exprsTok.nextToken().trim();
+		minutesExp = exprsTok.nextToken().trim();
+		hoursExp = exprsTok.nextToken().trim();
+		daysOfMonthExp = exprsTok.nextToken().trim();
+		monthsExp = exprsTok.nextToken().trim();
+		daysOfWeekExp = exprsTok.nextToken().trim();
+	}
+
+	public Set getSecondsSet()
+	{
+		return seconds;
+	}
+
+	public String getSecondsField()
+	{
+		return getExpressionSetSummary(seconds);
+	}
+
+	public Set getMinutesSet()
+	{
+		return minutes;
+	}
+
+	public String getMinutesField()
+	{
+		return getExpressionSetSummary(minutes);
+	}
+
+	public Set getHoursSet()
+	{
+		return hours;
+	}
+
+	public String getHoursField()
+	{
+		return getExpressionSetSummary(hours);
+	}
+
+	public Set getDaysOfMonthSet()
+	{
+		return daysOfMonth;
+	}
+
+	public String getDaysOfMonthField()
+	{
+		return getExpressionSetSummary(daysOfMonth);
+	}
+
+	public Set getMonthsSet()
+	{
+		return months;
+	}
+
+	public String getMonthsField()
+	{
+		return getExpressionSetSummary(months);
+	}
+
+	public Set getDaysOfWeekSet()
+	{
+		return daysOfWeek;
+	}
+
+	public String getDaysOfWeekField()
+	{
+		return getExpressionSetSummary(daysOfWeek);
+	}
+
+	public String getSecondsExp()
+	{
+		return secondsExp;
+	}
+
+	public String getMinutesExp()
+	{
+		return minutesExp;
+	}
+
+	public String getHoursExp()
+	{
+		return hoursExp;
+	}
+
+	public String getDaysOfMonthExp()
+	{
+		return daysOfMonthExp;
+	}
+
+	public String getMonthsExp()
+	{
+		return monthsExp;
+	}
+
+	public String getDaysOfWeekExp()
+	{
+		return daysOfWeekExp;
+	}
+}
